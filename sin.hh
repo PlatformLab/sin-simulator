f189e690d77126f9a7004d1eb0bbdb5d0c2d85c8
Greg Hill <gdh39@cornell.edu>
2015-04-17 16:57:13 -0700

    something basic

--- sin.hh
+++ sin.hh
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cassert>
+
 
 using namespace std;
 
@@ -15,9 +17,7 @@ struct flow {
     size_t num_packets;
 };
 
-static std::vector<slot> order_book(100, {1, "comcast"});
-//buy all until sent all, then move slots back until util optimized
-
+static std::vector<slot> order_book(10, {1, "comcast"});
 
 class Compare
 {
@@ -40,27 +40,46 @@ void make_bids(string user, struct flow flow)
         if (order_book.at(idx).owner != user)
         {
             tentative_order.push(std::make_pair( order_book.at(idx), idx));
+            cout << "tentatively bought " << idx << " for " <<order_book.at(idx).cost<< endl;
             cost += order_book.at(idx).cost;
         }
         idx++;
     }
-    value = idx; // time of last send
+    value = -(idx - 1); // time of last send
     // take most expensive packet and try to move it to maximize value-cost
 
     int cur_utility = (int) value - cost;
+    cout << "cur_utility is " << cur_utility << endl;
     while (idx < order_book.size())
     {
         const pair<struct slot, size_t> & top = tentative_order.top();
-        int move_utility = idx - (cost - top.first.cost + order_book.at(idx).cost);
+        int move_utility = -idx - (cost - top.first.cost + order_book.at(idx).cost);
+        cout << "utility diff of swapping most expensive slot to slot idx "<< idx << " is " << move_utility - cur_utility << endl;
         if (move_utility > cur_utility)
         {
-            cost -= top.first.cost + order_book.at(idx).cost;
+            cout << "swapping" << endl;
+            cost = cost - top.first.cost + order_book.at(idx).cost;
             tentative_order.pop();
             tentative_order.push( std::make_pair( order_book.at(idx), idx) );
 
             cur_utility = move_utility;
         }
+        idx++;
     }
+
+    int total_cost = 0;
+    // make bids
+    while (not tentative_order.empty())
+    {
+        const pair<struct slot, size_t> & top = tentative_order.top();
+        assert(order_book.at(top.second).cost == top.first.cost);
+        assert(order_book.at(top.second).owner == top.first.owner);
+        total_cost += order_book.at(top.second).cost;
+        order_book.at(top.second) = { 5, user };
+        tentative_order.pop();
+    }
+    cout << total_cost << " " << cost << endl;
+    assert( total_cost == cost);
 }
 
 int main(){

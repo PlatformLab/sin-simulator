2bbb894051123e61c916b37003bd31a079db9e1e
Greg Hill <gdh39@cornell.edu>
2015-04-21 16:26:06 -0700

    next ineration

--- sin.hh
+++ sin.hh
@@ -9,6 +9,7 @@ using namespace std;
 struct user {
     string name;
     int flow_size;
+    int cur_completion_time;
     int money;
 };
 
@@ -40,12 +41,43 @@ priority_queue<size_t> get_positions(struct user user)
     return ret;
 }
 
-void make_bid(struct user user)
+bool buy_best_slot(struct user &user)
 {
-    priority_queue<size_t> positions = get_positions(user);
-    int unassigned_packets = user.flow_size - positions.size();
+    int best_slot_idx = -1;
+    int best_slot_utility = -11111;
 
+    for (int i = 0; i < order_book.size(); i++)
+    {
+        auto &slot_i = order_book.at(i);
+        if (slot_i.owner != user.name) {
+            int slot_i_utility = -i - slot_i.cost;
+            if (slot_i_utility > best_slot_utility) {
+                best_slot_utility = slot_i_utility;
+                best_slot_idx = i;
+            }
+        }
+    }
+    if (best_slot_idx != -1) {
+        order_book.at(best_slot_idx).owner = user.name;
+        std::cout << "bought slot " << best_slot_idx << " for " << order_book.at(best_slot_idx).cost << endl;
+        user.money -= order_book.at(best_slot_idx).cost;
+        order_book.at(best_slot_idx).cost++; // charge more for it
+        return true;
+    } 
+    // couldnt buy
+    return false;
+}
 
+void make_bids(struct user &user)
+{
+    int start_money = user.money;
+    std::cout << "making bids for " << user.name << endl;
+    for (int i = user.flow_size; i > 0; i--)
+    {
+        bool success = buy_best_slot(user);
+        assert(success);
+    }
+    std::cout << user.name << " finished bidding, spent " << start_money-user.money << endl;
 }
 
 /*
@@ -105,12 +137,10 @@ void make_bids(string user, struct flow flow)
 */
 
 int main(){
-    struct user greg{"greg", 3, 100};
-    struct user keith{"keith", 4, 100};
-    /*
-    make_bids("greg", a);
-    make_bids("keith", b);
-    */
+    struct user gregs{"gregs", 3, -11111, 100};
+    struct user keith{"keith", 4, -11111, 100};
+    make_bids(gregs);
+    make_bids(keith);
 
     std::cout << "hello world" << std::endl;
     return 1;

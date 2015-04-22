de1818210897015c33c5306c5651e7d01ce72f5c
Greg Hill <gdh39@cornell.edu>
2015-04-21 17:21:26 -0700

    stuff

--- sin.hh
+++ sin.hh
@@ -3,7 +3,6 @@
 #include <queue>
 #include <cassert>
 
-
 using namespace std;
 
 struct user {
@@ -15,32 +14,55 @@ struct user {
 
 struct slot {
     int cost;
-    string owner;
+    user* owner;
 };
 
-static std::vector<slot> order_book(10, {1, "comcast"});
+static struct user comcast{"ccast", -1, -1, 0};
+static std::vector<slot> order_book(9, {1, &comcast});
+
+void print_order_book()
+{
+    int i = 0;
+    cout << "[";
+    for (auto &slot : order_book)
+    {
+        cout << i++ << ":" << slot.owner->name << "$" << slot.cost << " | "; 
+    }
+    cout << "]" << endl;
+}
 
 bool buy_best_slot(struct user &user)
 {
     int best_slot_idx = -1;
     int best_slot_utility = -11111;
 
-    for (int i = 0; i < order_book.size(); i++)
+    for (int i = 0; (size_t) i < order_book.size(); i++)
     {
         auto &slot_i = order_book.at(i);
-        if (slot_i.owner != user.name) {
-            int slot_i_utility = -i - slot_i.cost;
+        int flow_completion_time = max(i, user.cur_completion_time);
+        if (slot_i.owner->name != user.name) {
+            int slot_i_utility = -flow_completion_time - slot_i.cost;
+            cout << "i " << i << " utility " << slot_i_utility << endl;
             if (slot_i_utility > best_slot_utility) {
                 best_slot_utility = slot_i_utility;
                 best_slot_idx = i;
             }
+        } else {
+            cout << "couldnt buy slot " << i << ": already owned" << endl;
+
         }
     }
     if (best_slot_idx != -1) {
-        order_book.at(best_slot_idx).owner = user.name;
-        std::cout << "bought slot " << best_slot_idx << " for " << order_book.at(best_slot_idx).cost << endl;
         user.money -= order_book.at(best_slot_idx).cost;
+        order_book.at(best_slot_idx).owner->money += order_book.at(best_slot_idx).cost;
+
+        order_book.at(best_slot_idx).owner = &user;
+
+        std::cout << "bought slot " << best_slot_idx << " for " << order_book.at(best_slot_idx).cost << endl;
         order_book.at(best_slot_idx).cost++; // charge more for it
+
+        user.cur_completion_time = max(user.cur_completion_time, best_slot_idx);
+        std::cout << "completion time now " << user.cur_completion_time << endl;
         return true;
     } 
     // couldnt buy
@@ -56,7 +78,10 @@ void make_bids(struct user &user)
         bool success = buy_best_slot(user);
         assert(success);
     }
-    std::cout << user.name << " finished bidding, spent " << start_money-user.money << endl;
+
+    std::cout << user.name << " finished bidding, spent " << start_money-user.money <<
+        " and flow finishes at time " << user.cur_completion_time << endl;
+        print_order_book();
 }
 
 int main(){
@@ -65,6 +90,5 @@ int main(){
     make_bids(gregs);
     make_bids(keith);
 
-    std::cout << "hello world" << std::endl;
     return 1;
 }

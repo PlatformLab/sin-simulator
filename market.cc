341535906ce73162a237678d25ca172af7542a3c
Greg Hill <gdh39@cornell.edu>
2015-04-30 12:45:24 -0700

    basic

--- market.cc
+++ market.cc
@@ -2,20 +2,40 @@
 
 using namespace std;
 
-void Market::add_bid(struct User* user, uint32_t slot_time, uint32_t price)
+void Market::add_bid(struct User* user, uint64_t slot_time, uint32_t price)
 {
 
 }
 
-bool Market::delete_bid(struct User* user, uint32_t slot_time)
+bool Market::delete_bid(struct User* user, uint64_t slot_time)
 {
 return false;
 }
 
-void Market::add_offer(struct User* user, uint32_t slot_time, uint32_t price)
+void Market::add_offer(struct User* user, uint64_t slot_time, uint32_t price)
 {
 }
 
-void Market::add_packet(struct User* user, uint32_t slot_time, string* packet)
+void Market::add_packet(struct User* user, uint64_t slot_time, string* packet)
 {
+}
+
+void Market::advance_time()
+{
+    cur_time++;
+    assert(order_book.front().time == cur_time);  
+
+    if (order_book.front().packet) {
+        cout << "sending packet at time " << cur_time << " with contents: " << *order_book.front().packet << endl;
+    } else {
+        cout << "no packet to send at time " << cur_time << endl;
+    }
+    order_book.pop();
+    //struct slot to_add{
+    //order_book.emplace_back();
+}
+
+std::queue<struct Slot> Market::get_order_book()
+{
+    return order_book;
 }
--- market.hh
+++ market.hh
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <deque>
+#include <queue>
 #include <vector>
 #include <cassert>
 
@@ -13,21 +13,33 @@ struct Bid {
 };
 
 struct Slot {
-    uint32_t time;
+    public:
+    uint64_t time;
     struct User* owner;
     uint32_t current_offer;
+
+    //private:
     std::vector<struct Bid> bids;
     std::string* packet;
 };
 
 
 class Market {
-    std::deque<Slot> order_book;
+    private:
+    std::queue<struct Slot> order_book;
+    uint64_t next_time_to_add;
+    uint64_t cur_time;
 
-    void add_bid(struct User* user, uint32_t slot_time, uint32_t price);
-    bool delete_bid(struct User* user, uint32_t slot_time);
+    public:
+    void add_bid(struct User* user, uint64_t slot_time, uint32_t price);
 
-    void add_offer(struct User* user, uint32_t slot_time, uint32_t price);
+    bool delete_bid(struct User* user, uint64_t slot_time);
 
-    void add_packet(struct User* user, uint32_t slot_time, std::string* packet);
+    void add_offer(struct User* user, uint64_t slot_time, uint32_t price);
+
+    void add_packet(struct User* user, uint64_t slot_time, std::string* packet);
+
+    void advance_time();
+
+    std::queue<struct Slot> get_order_book();
 };

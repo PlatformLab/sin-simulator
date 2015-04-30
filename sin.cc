2ef923eb3358db0ee9575446ba4f65563b2c252a
Greg Hill <gdh39@cornell.edu>
2015-04-30 14:53:02 -0700

    nice second by second advance

--- sin.cc
+++ sin.cc
@@ -8,8 +8,16 @@ using namespace std;
 int main(){
     cout << "hello world" << endl;
     Market mkt(30, 1);
+
+    time_t last_time = 0;
+    time_t cur_time = time(nullptr);
     while (not mkt.get_order_book().empty()) {
-        mkt.advance_time();
+        cur_time = time(nullptr);
+        // advance time if we havent already for that same time
+        if (cur_time != last_time) {
+            mkt.advance_time();
+            last_time = cur_time;
+        }
     }
     return 1;
 }

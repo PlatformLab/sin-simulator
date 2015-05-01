78adf11a293d6ff31ecfc59a57d5bbe1426f3dc6
Greg Hill <gdh39@cornell.edu>
2015-04-30 20:32:31 -0700

    works!

--- noop_user.hh
+++ noop_user.hh
@@ -1,6 +1,7 @@
 #ifndef NOOP_USER
 #define NOOP_USER
 
+#include <iostream>
 #include "abstract_user.hh"
 
 class NoopUser : public AbstractUser
@@ -8,6 +9,7 @@ class NoopUser : public AbstractUser
     public:
     struct User_actions get_actions(std::vector<struct Slot_view> &given_view) 
     {
+        std::cout << "In noop user!" << std::endl;
         return {};
     }
 };

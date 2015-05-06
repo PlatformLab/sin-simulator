83a687e418eb0317283330e7c7f7d3927106d10a
Greg Hill <gdh39@cornell.edu>
2015-05-06 11:09:21 -0700

    doesnt work

--- basic_user.hh
+++ basic_user.hh
@@ -4,34 +4,58 @@
 class BasicUser : public AbstractUser
 {
     const std::string name;
-    const uint32_t flow_size;
+    const size_t flow_size;
 
     public:
-        BasicUser(std::string name, uint32_t flow_size) : name(name), flow_size(flow_size) {}
+        BasicUser(std::string name, size_t flow_size) : name(name), flow_size(flow_size) {}
 
         void take_actions(struct Market& mkt)
         {
-            auto &order_book = mkt.get_order_book();
+            get_best_slots(mkt.get_order_book(), flow_size);
 
             std::cout << "I'm a basic user named " << name;
             std::cout << " with a flow of size " << flow_size << std::endl;
         }
 
 
-    /*
-    private:
-    int get_best_slots(std::deque<struct Slot> &order_book, size_t start, size_t finish, size_t n,
-    std::vector<size_t> idxs)
+    void get_best_slots(std::deque<struct Slot> &order_book, size_t flow_size)
     {
-        int best_utility = -11111;
-        for (size_t i = start; i < finish-n; i++)
+        std::vector<size_t> idxs_to_buy;
+        recursive_slot_checker(order_book, 0, order_book.size(), flow_size, idxs_to_buy);
+       
+        std::cout << "got idxs to buy ";
+        for (auto i : idxs_to_buy)
         {
-            int utility = -order_book.at(i).
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
 
-            
-
+    private:
+    int recursive_slot_checker(std::deque<struct Slot> &order_book, size_t start, size_t finish, size_t n,
+    std::vector<size_t> &idxs)
+    {
+        // WE CANT OWN SLOT
+        // base case
+        if (n == 1)
+        {
+            idxs.emplace_back(start);
+            return -order_book.at(start).lowest_offer().cost - start; // cost - flow completion time
         }
 
-
-*/
+        int best_utility = -11111;
+        std::vector<size_t> &best_idxs = idxs;
+        for (size_t i = start; i < finish-n; i++)
+        {
+            std::vector<size_t> recursive_idxs(idxs);
+            int utility = -order_book.at(i).lowest_offer().cost
+                + recursive_slot_checker(order_book, i+1, finish, n-1, recursive_idxs);
+            if (utility > best_utility) {
+                best_utility = utility;
+                best_idxs = idxs;
+            }
+        }
+        //assert(best_utility != -11111)
+        return best_utility;
+    }
 };

7a3be9809c178191bb5a7f2d26724b455bc16b22
Greg Hill <gdh39@cornell.edu>
2015-06-12 11:12:22 -0700

    transient commit, changing pq type

--- src/flow_completion_time_user.cc
+++ src/flow_completion_time_user.cc
@@ -13,7 +13,9 @@ FlowCompletionTimeUser::FlowCompletionTimeUser( const std::string &name, const s
 
 static vector<size_t> idxs_to_buy( const deque<SingleSlot> &order_book, const string &name, size_t start_time, size_t num_packets_to_buy, const size_t latest_time_already_owned )
 {
-    priority_queue<size_t> idxs_to_buy {};
+    //auto cmp = [](const size_t &a, const size_t &b) const{ return a > b; };
+    vector<size_t> v;
+    priority_queue<size_t, vector<size_t>, greater<size_t>> idxs_to_buy(greater<size_t>(), v);
     double idxs_to_buy_cost = 0;
 
     size_t idx = start_time < order_book.front().time ? 0 : order_book.front().time - start_time;
@@ -32,8 +34,9 @@ static vector<size_t> idxs_to_buy( const deque<SingleSlot> &order_book, const st
     idx--; // we incremented 1 too many times in while loop
 
     size_t min_flow_completion_time = max(order_book.at( idx ).time, latest_time_already_owned);
+    cout << name << " got min fct " <<  min_flow_completion_time << endl;
 
-    priority_queue<size_t> best_idxs = idxs_to_buy;
+    priority_queue<size_t> best_idxs {};//= idxs_to_buy;
     double flow_benefit = -(min_flow_completion_time - start_time);
     double best_utility = flow_benefit - idxs_to_buy_cost;
 
@@ -48,11 +51,12 @@ static vector<size_t> idxs_to_buy( const deque<SingleSlot> &order_book, const st
             idxs_to_buy.pop();
             assert(idxs_to_buy.size() == num_packets_to_buy);
 
-            double current_benefit = -max( i, latest_time_already_owned ) - start_time;
-            double current_utility = current_benefit - idxs_to_buy_cost;
+            size_t current_benefit = -max( potential_slot.time, latest_time_already_owned ) - start_time;
+            double current_utility = (double) current_benefit - idxs_to_buy_cost;
+            cout << "benefit for " << i << " is " << current_benefit << endl;
 
             if (current_utility > best_utility) {
-                best_idxs = idxs_to_buy;
+                //TODO best_idxs = idxs_to_buy;
                 best_utility = current_utility;
             }
         }
@@ -130,23 +134,30 @@ void FlowCompletionTimeUser::take_actions( Market& mkt )
             mkt.clear_offers_from_slot( idx, name_ );
 
             // don't count slot we are selling for flow completion time
+            size_t cur_flow_completion_time = 0;
             size_t flow_completion_time_if_sold = 0;
             for ( size_t i = 0; i < order_book.size(); i++) {
-                if ( i != idx and order_book.at(i).owner == name_ ) {
-                    flow_completion_time_if_sold = order_book.at(i).time;
+                if ( order_book.at(i).owner == name_ ) {
+                    cur_flow_completion_time = order_book.at(i).time; // TODO dont calc this every time
+                    if ( i != idx ) {
+                        flow_completion_time_if_sold = order_book.at(i).time;
+                    }
                 }
             }
+            cout << "got fct if sold " << flow_completion_time_if_sold << " for " << idx << endl;
 
             num_packets_to_buy = 1;
             size_t idx_would_buy_instead = idxs_to_buy( order_book, name_, flow_start_time_, num_packets_to_buy, flow_completion_time_if_sold ).front();
+            cout << "would buy " << idx_would_buy_instead << " instead of " << idx;
             const SingleSlot &slot_would_buy_instead = order_book.at( idx_would_buy_instead );
 
             // if the time of slot we buy instead is later than the last time if we sold, then it decreases benefit
-            double benefit_delta = min( 0., (double) flow_completion_time_if_sold - (double) slot_would_buy_instead.time );
+            double benefit_delta = min( 0., (double) cur_flow_completion_time - (double) slot_would_buy_instead.time );
 
             double utility_delta = benefit_delta - slot_would_buy_instead.best_offer().cost;
 
             mkt.add_offer_to_slot( idx, { -utility_delta + .01, name_ } );
+            cout << " pricing it at " << -utility_delta + .01 << endl;
 
             idx++;
         }

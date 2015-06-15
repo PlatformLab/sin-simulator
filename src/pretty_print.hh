10beb11cbd073549e31bce3bdfb61a2aae691102
Greg Hill <gdh39@cornell.edu>
2015-06-15 12:27:52 -0700

    better printing

--- src/pretty_print.hh
+++ src/pretty_print.hh
@@ -4,6 +4,7 @@
 #define PRETTY_PRINT_HH
 
 #include <iostream>
+#include <limits>
 
 template <typename T>
 void printPacketsSent(T& container)
@@ -21,31 +22,48 @@ void printPacketsSent(T& container)
 }
 
 template <typename T>
+void print_consecutive_slot_range(T *start, T* end)
+{
+    std::cout << start->time << "-" << end->time << ": ";
+
+    if (start->owner != "") {
+        std::cout << start->owner;
+    } else {
+        std::cout << "null";
+    }
+
+    std::cout << " $";
+    if ( start->has_offers() ) {
+        std::cout << start->best_offer().cost;
+    } else {
+        std::cout << "null";
+    }
+}
+
+template <typename T>
 void printOrderBook(T& container)
 {
     std::cout << "[ ";
-    bool is_first = true;
-    for (const auto &slot : container)
+    auto *first_consecutive_slot = &container.front(); 
+    auto *last_consecutive_slot = &container.front(); 
+
+    for (auto &slot : container)
     {
-        if (not is_first) {
+        bool owner_matches = slot.owner == last_consecutive_slot->owner;
+        bool cost_matches = last_consecutive_slot->has_offers() == slot.has_offers();
+        if ( cost_matches and slot.has_offers() ) {
+            cost_matches = last_consecutive_slot->best_offer().cost == slot.best_offer().cost;
+        }
+
+        if ( not owner_matches or not cost_matches ) {
+            print_consecutive_slot_range( first_consecutive_slot, last_consecutive_slot );
             std::cout << " | ";
-        }
-        std::cout << slot.time << ". ";
-        if (slot.owner != "") {
-            std::cout << slot.owner;
-        } else {
-            std::cout << "null";
-        }
 
-        std::cout << " $";
-        if (slot.has_offers()) {
-            std::cout << slot.best_offer().cost;
-        } else {
-            std::cout << "null";
+            first_consecutive_slot = &slot;
         }
-
-        is_first = false;
+        last_consecutive_slot = &slot;
     }
+    print_consecutive_slot_range( first_consecutive_slot, last_consecutive_slot );
     std::cout << " ]" << std::endl;
 }
 

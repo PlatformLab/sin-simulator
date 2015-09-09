75c53864b7fb57d5f98fc7fff8d8a7075da93339
Greg Hill <wazdee@gmail.com>
2015-09-08 21:52:10 -0700

    gen data script progess meter

--- src/plot/gen_data.sh
+++ src/plot/gen_data.sh
@@ -1,13 +1,16 @@
-SIN_LOCATION=../sin
+
 MEAN_DELAY_FILE=mean_delay.dat
 
-NUM_TRIALS=4
+NUM_TRIALS=16
 DIE_SIZE=40
 
+echo -n "finished num-flows= "
 echo '"num flows" "SIN" "SRTF" "Rate Sharing" ' > $MEAN_DELAY_FILE
 for i in 2 3 4 6 8 16; #32;
     do
         echo -n "$i " >> $MEAN_DELAY_FILE
         TIMES_TO_RUN=$(($NUM_TRIALS*32/$i))
         $SIN_LOCATION --num-flows=$i --num-trials=$TIMES_TO_RUN --die-size=$DIE_SIZE >> $MEAN_DELAY_FILE
+        echo -n "$i, "
     done
+echo "done"

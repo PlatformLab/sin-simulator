79dc54a364e44d4a0d43c1ac3523597f2ac2f459
Greg Hill <wazdee@gmail.com>
2015-09-11 16:50:07 -0700

    minor change

--- src/link.hh
+++ src/link.hh
@@ -16,7 +16,7 @@ struct Link {
     std::vector<Interval> get_intervals() const
     {
         std::vector<Interval> toRet;
-        for ( int i = 0; i < num_intervals; i++ ) {
+        for ( size_t i = 0; i < num_intervals; i++ ) {
             toRet.push_back( { uid, i, i+propagation_time, default_interval_cost } );
         }
         return toRet;

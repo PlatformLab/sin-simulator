ea093231e742eb09977c7e7e0b5c30dbfe3acba9
Greg Hill <wazdee@gmail.com>
2015-09-17 18:25:44 -0700

    offer comment, proper equals and hash

--- src/offer.hh
+++ src/offer.hh
@@ -10,10 +10,10 @@ struct Offer {
     size_t seller_uid;
     Interval interval;
     double cost;
-    std::pair<bool, Interval> exchange_for_interval;
+    std::pair<bool, Interval> exchange_for_interval; /* if first bool true, this offer's cost includes trading for the following interval */
 
     bool operator==( const Offer& other ) const {
-        return seller_uid == other.seller_uid and interval == other.interval and cost == other.cost;
+        return seller_uid == other.seller_uid and interval == other.interval and cost == other.cost and exchange_for_interval == other.exchange_for_interval;
     }
 };
 
@@ -26,9 +26,11 @@ namespace std {
                 using std::size_t;
                 using std::hash;
 
-                return ( hash<size_t>()(o.seller_uid) )
-                    ^ ( hash<Interval>()(o.interval) + 1 )
-                    ^ ( hash<double>()(o.cost) + 2 );
+                return ( hash<size_t>()( o.seller_uid ) )
+                    ^ ( hash<Interval>()( o.interval ) + 1 )
+                    ^ ( hash<double>()( o.cost) + 2 )
+                    ^ ( hash<bool>()( o.exchange_for_interval.first ) + 3 )
+                    ^ ( hash<Interval>()( o.exchange_for_interval.second ) + 4 );
             }
         };
 }
